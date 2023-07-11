6042641ae34c3ced7c4bddbe2a24518038da32ec
RoderickJ <114635654+JRodrigoHC19@users.noreply.github.com>
2023-07-11 06:10:06 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>Booking Hive</title> 
 
     <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('storage/sass/detalles.scss') }}">
     <link rel="stylesheet" href="{{ asset('storage/css/colores.css') }}">
