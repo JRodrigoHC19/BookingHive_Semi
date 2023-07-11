9436b3654c51e0b3448f8d83ef6b344b404440d8
RoderickJ <114635654+JRodrigoHC19@users.noreply.github.com>
2023-07-11 06:21:55 -0500

    Update graficas.blade.php

--- resources/views/hotel/graphics/graficas.blade.php
+++ resources/views/hotel/graphics/graficas.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('storage/sass/detalles.scss') }}">
     <link rel="stylesheet" href="{{ asset('storage/css/colores.css') }}">
     <style>
@@ -350,7 +351,7 @@
     
     <!-- ADIICIONALES -->
     <script src="{{ asset('storage/js/animaciones.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <!-- <script src="sweetalert2/dist/weetalert2.all.min.js"></script> -->
     
    
