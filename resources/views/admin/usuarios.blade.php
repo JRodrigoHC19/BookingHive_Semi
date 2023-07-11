e74a893700511f9c28f2d29217908c011d0d7197
RoderickJ <114635654+JRodrigoHC19@users.noreply.github.com>
2023-07-11 06:36:38 -0500

    Update usuarios.blade.php

--- resources/views/admin/usuarios.blade.php
+++ resources/views/admin/usuarios.blade.php
@@ -265,10 +265,9 @@
     <!-- ADIICIONALES -->
     <script src="{{ asset('storage/js/animaciones.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <!-- <script src="sweetalert2/dist/weetalert2.all.min.js"></script> -->
-
-    <!-- NO SE PARA QUE SERÁ -->
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
+    
 </body>
 </html>
 
